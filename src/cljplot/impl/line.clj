8346ac252eb50c4e44d60a6c78214d3f7545d274
tom <tom@that-place>
2019-05-12 20:26:12 -0500

    Minor patch to density data prep

--- src/cljplot/impl/line.clj
+++ src/cljplot/impl/line.clj
@@ -88,8 +88,8 @@
 (defmethod prepare-data :density [_ data {:keys [kernel-bandwidth margins] :as conf}]
   (let [dens-data (extract-first data)
         f (if kernel-bandwidth
-            (stats/kernel-density dens-data kernel-bandwidth)
-            (stats/kernel-density dens-data))
+            (stats/kernel-density :default dens-data kernel-bandwidth)
+            (stats/kernel-density :default dens-data))
         with-domain (assoc conf :domain (extend-domain-numerical (take 2 (stats/extent dens-data)) (or (:x margins) [0 0])))]
     [with-domain (prepare-data :function f with-domain)]))
 
